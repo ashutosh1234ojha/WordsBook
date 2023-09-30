e35af47c83aefc12f135ec7e069aed478fed210c
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2023-09-30 13:24:10 +0530

    bg color update and padding changes

--- app/src/main/java/com/example/mywordsbook/HomeScreen.kt
+++ app/src/main/java/com/example/mywordsbook/HomeScreen.kt
@@ -1,6 +1,8 @@
 package com.example.mywordsbook
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -21,11 +24,14 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.mywordsbook.db.Word
 
 @Composable
 fun HomeScreen(
@@ -37,16 +43,8 @@ fun HomeScreen(
 
         LazyColumn(modifier = Modifier.weight(1f)) {
             items(list) { item ->
-                Text(text = item.wording, fontSize = 18.sp, color = Color.Black)
 
-                Text(text = item.meaning, fontSize = 16.sp, color = Color.Gray)
-                Spacer(
-                    modifier = Modifier
-                        .padding(top = 5.dp, bottom = 10.dp)
-                        .fillMaxWidth()
-                        .height(1.dp)
-                        .background(color = Color.Black)
-                )
+                ItemList(item)
             }
         }
 
@@ -65,4 +63,26 @@ fun HomeScreen(
         }
 
     }
+}
+
+@Composable
+fun ItemList(item: Word) {
+    Column(
+        modifier = Modifier
+            .padding(bottom = 5.dp)
+            .fillMaxWidth()
+            .clip(shape = RoundedCornerShape(10.dp))
+            .background(Color(0xFFADDFAD))
+            .padding(10.dp)
+
+    ) {
+        Text(text = item.wording, fontSize = 18.sp, color = Color.Black)
+
+        Text(text = item.meaning, fontSize = 16.sp, color = Color.DarkGray)
+
+
+
+    }
+
+
 }
