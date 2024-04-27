b7570d16e26b47dc6a5368f5847a9ae05665bf39
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2024-04-27 20:23:23 +0530

    no quiz error handled

--- app/src/main/java/com/example/mywordsbook/QuizScreen.kt
+++ app/src/main/java/com/example/mywordsbook/QuizScreen.kt
@@ -2,6 +2,7 @@ package com.example.mywordsbook
 
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -9,7 +10,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -29,6 +32,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -82,82 +86,107 @@ fun QuizScreen(navController: NavHostController, commonViewModel: CommonViewMode
 
         }
     }
-    HorizontalPager(state = pagerState) { page ->
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        if (firstFour.size >= 4) {
+            HorizontalPager(state = pagerState) { page ->
 
-        Column {
-            Row(
-                modifier = Modifier
-                    .align(alignment = Alignment.End)
-                    .padding(10.dp)
-            ) {
-                Text(
-                    text = "Score ${myState}",
-                    style = TextStyle(
-                        fontWeight = FontWeight.Bold,
-                        color = Color.Green,
-                        fontSize = 20.sp
-                    )
-                )
-            }
-            Card(
-                modifier = Modifier
-                    .padding(10.dp)
-                    .fillMaxWidth()
-                    .clip(shape = RoundedCornerShape(5.dp, 5.dp, 5.dp, 5.dp))
-                    .background(Color(0xFFF5F5DC))
-                    .padding(vertical = 50.dp)
-            ) {
-                Text(
-                    modifier = Modifier.background(Color.Transparent),
-                    text = buildAnnotatedString {
-                        append("What the meaning of ")
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Blue,
-                                fontWeight = FontWeight.Bold
+                Column {
+                    Row(
+                        modifier = Modifier
+                            .align(alignment = Alignment.End)
+                            .padding(10.dp)
+                    ) {
+                        Text(
+                            text = "Score ${myState}",
+                            style = TextStyle(
+                                fontWeight = FontWeight.Bold,
+                                color = Color.Green,
+                                fontSize = 20.sp
                             )
-                        ) {
-                            append(word?.wording ?: "")
-                        }
-                        append("?")
-                    },
-                    style = TextStyle(
-                        fontWeight = FontWeight.Normal,
+                        )
+                    }
+                    Card(
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .fillMaxWidth()
+                            .clip(shape = RoundedCornerShape(5.dp, 5.dp, 5.dp, 5.dp))
+                            .background(Color(0xFFF5F5DC))
+                            .padding(vertical = 50.dp)
+                    ) {
+                        Text(
+                            modifier = Modifier.background(Color.Transparent),
+                            text = buildAnnotatedString {
+                                append("What the meaning of ")
+                                withStyle(
+                                    style = SpanStyle(
+                                        color = Color.Blue,
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                ) {
+                                    append(word?.wording ?: "")
+                                }
+                                append("?")
+                            },
+                            style = TextStyle(
+                                fontWeight = FontWeight.Normal,
+                                color = Color.Black,
+                                fontSize = 20.sp, textAlign = TextAlign.Center
+                            )
+                        )
+
+
+                    }
+
+                    QuizOption(0, option = listWordsNew?.get(0)?.meaning ?: "") {
+                        selectedOption.value = listWordsNew?.get(0)?.meaning ?: ""
+                        commonViewModel.updateScore(selectedOption.value == word?.meaning)
+
+
+                    }
+                    QuizOption(1, option = listWordsNew?.get(1)?.meaning ?: "") {
+                        selectedOption.value = listWordsNew?.get(1)?.meaning ?: ""
+                        commonViewModel.updateScore(selectedOption.value == word?.meaning)
+
+
+                    }
+                    QuizOption(2, option = listWordsNew?.get(2)?.meaning ?: "") {
+                        selectedOption.value = listWordsNew?.get(2)?.meaning ?: ""
+                        commonViewModel.updateScore(selectedOption.value == word?.meaning)
+
+
+                    }
+                    QuizOption(3, option = listWordsNew?.get(3)?.meaning ?: "") {
+                        selectedOption.value = listWordsNew?.get(3)?.meaning ?: ""
+                        commonViewModel.updateScore(selectedOption.value == word?.meaning)
+
+
+                    }
+
+                }
+
+
+            }
+
+        } else {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    painterResource(R.drawable.add_more_words),
+                    contentDescription = "Shuffle",
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(100.dp)
+                )
+
+                Text(
+                    text = "Please add at least four words for\n the quiz", style = TextStyle(
+                        fontWeight = FontWeight.Bold,
                         color = Color.Black,
                         fontSize = 20.sp, textAlign = TextAlign.Center
                     )
                 )
-
-
-            }
-
-            QuizOption(0, option = listWordsNew?.get(0)?.meaning ?: "") {
-                selectedOption.value = listWordsNew?.get(0)?.meaning ?: ""
-                commonViewModel.updateScore(selectedOption.value == word?.meaning)
-
-
-            }
-            QuizOption(1, option = listWordsNew?.get(1)?.meaning ?: "") {
-                selectedOption.value = listWordsNew?.get(1)?.meaning ?: ""
-                commonViewModel.updateScore(selectedOption.value == word?.meaning)
-
-
-            }
-            QuizOption(2, option = listWordsNew?.get(2)?.meaning ?: "") {
-                selectedOption.value = listWordsNew?.get(2)?.meaning ?: ""
-                commonViewModel.updateScore(selectedOption.value == word?.meaning)
-
-
-            }
-            QuizOption(3, option = listWordsNew?.get(3)?.meaning ?: "") {
-                selectedOption.value = listWordsNew?.get(3)?.meaning ?: ""
-                commonViewModel.updateScore(selectedOption.value == word?.meaning)
-
-
             }
 
         }
-
 
     }
 
