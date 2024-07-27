a6d3c5f5fd15bcd72a11cb67c928f9a90d3f625d
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2024-07-27 11:03:31 +0530

    gradle and manifest update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    val room_version = "2.5.2"
+    val room_version = "2.6.1"
 //    implementation("androidx.room:room-runtime:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
 
